6ba889624d5491e6e09f441cbf3b693cf354e53a
rmontesgamez <rmontesgamez@hotmail.com>
2023-03-24 17:13:45 +0000

    p`r2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # import pandas as pd
 
 if __name__ == "__main__":
-    print("File one executed when ran directly")
+    print("File one executed when ran directlynnn")
 else:
     print("File one executed when imported")
+print("hola")
